81a1ee739ecf36cbc2c42d9d2e08ed5f8fcc4030
AforArdi <binsbro36@gmail.com>
2026-02-07 00:19:32 +0600

    gono vote

--- rough.js
+++ rough.js
@@ -1,8 +1,25 @@
-let number = 17.17;
-
-function findType(num) {
-    if (typeof num === 'number') {
+function gonoVote(array) {
+    //write your code here
+    if (!Array.isArray(array)){
+        return 'Invalid'
+    }
+    let haCount = 0;
+    let naCount = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === 'ha') {
+            haCount++;
+        }
+        if (array[i] === 'na') {
+            naCount++;
+        }
+    }
+    if (haCount > naCount) {
         return true;
     }
+    if (haCount < naCount) {
+        return false;
+    }
+    else {
+        return 'equal';
+    }
 }
-console.log(findType(number));
